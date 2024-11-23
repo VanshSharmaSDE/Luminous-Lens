19fe6d10b87b925a20e283cd36f98f95c769f6ba
Vansh Sharma <vansh4664@gmail.com>
2024-11-23 21:14:43 +0530

    Update Login.jsx

--- LuminousLens/src/components/Login.jsx
+++ LuminousLens/src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/users/login', credentials);
+      const res = await axios.post(`${apiUrl}/users/login`, credentials);
       localStorage.setItem('token', res.data.token);
       navigate('/dashboard');
     } catch (err) {
